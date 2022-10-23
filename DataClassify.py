fb2efe106141dd5006134084dc073f8dc10e8a7a
byjqxx <zhlfqcrwxh@gmail.com>
2022-10-23 19:45:23 +0800

    数字分类，加注释

--- DataClassify.py
+++ DataClassify.py
@@ -12,18 +12,26 @@ https://blog.csdn.net/juge0007/article/details/82223053
 
 
 nums = input()
+# c 分类依据的数
 c = int(nums.split()[0])
+# b 模数（模运算，即取余）
 b = int(nums.split()[1])
 data = [ int(n) for n in nums.split()[2:] ]
 
 dict1 = {}
 for i in data:
+    # 将i转换为16进制的数字，去掉0x之后，补够8位数
     i_ = hex(i)[2:].zfill(8)
     temp = 0
 
+    # 将转换后的16进制数，每2个分开，当成一个16进制数字，
+    # 然后再将这些16进制的数累加，转换成10进制整数
     for i in range(0, len(i_), 2):
         temp += int(i_[i:i+2], 16)
 
+    # 将累加的和与b进行模运算，余数为t
+    # 将余数和c比较，对输入的数分类，如果大于等于c则是一个无效的数字，如果小于c则有效。
+    # 最后记录下有效数字的个数
     t = temp % b
     if t < c:
         if t in dict1:
@@ -31,6 +39,7 @@ for i in data:
         else:
             dict1[t] = 1
 
+# 如果dict1不为空，说明有效数字个数不为1，输出有效数字个数
 if dict1:
     print(max(dict1.values()))
 else:
